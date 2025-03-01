ec3729a7dbbd6a7bec811695d83f222164239d7f
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2025-03-01 15:22:56 +0100

    Add Plausible Analytics integration with custom domain configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Poppins, Sora } from 'next/font/google';
-import { TranslationProvider } from '../hooks/TranslationProvider';
+import type { Metadata } from "next";
+import PlausibleProvider from "next-plausible";
+import { Poppins, Sora } from "next/font/google";
+import { TranslationProvider } from "../hooks/TranslationProvider";
+import "./globals.css";
 
 // Initialize the Poppins font
 const poppins = Poppins({
-  weight: ['300', '400', '500'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
 });
 
 // Initialize the Sora font
 const sora = Sora({
-  weight: ['300', '400', '500', '600'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-sora',
+  weight: ["300", "400", "500", "600"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-sora",
 });
 
 export const metadata: Metadata = {
-  title: 'lulo - Innovation Made Simple',
-  description: 'How can we help bring your ideas to life? Share your vision with us, and we’ll get back to you shortly with AI-powered solutions designed for impact.',
+  title: "lulo - Innovation Made Simple",
+  description:
+    "How can we help bring your ideas to life? Share your vision with us, and we’ll get back to you shortly with AI-powered solutions designed for impact.",
   icons: {
-    icon: '/favicon.ico',
+    icon: "/lulo.png",
   },
 };
 
@@ -34,11 +36,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.variable} ${sora.variable}`}>
-      <body suppressHydrationWarning={true}>
-        <TranslationProvider>
-          {children}
-        </TranslationProvider>
-      </body>
+      <PlausibleProvider
+        domain="luloai.com"
+        customDomain="https://plausible-analytics.yesidlopez.de"
+      >
+        <body suppressHydrationWarning={true}>
+          <TranslationProvider>{children}</TranslationProvider>
+        </body>
+      </PlausibleProvider>
     </html>
   );
 }
